53496c892bf132e0d7dbd48e437cc4ecca1cba54
Sahil <125720378+Sahilahmad36@users.noreply.github.com>
2023-05-08 18:13:53 +0530

    Deployed website

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -8,39 +8,47 @@ function Blog() {
 
 <div className="blog-articles">
 
-   
-    <article className="post-articles" style={{borderTop: "2px solid #81D8F7"}}>
+<article className="post-articles" style={{borderTop: "2px solid #08fdd8"}}>
         <div className="post-content">
-            <time className="post-date" style={{color:"#81D8F7"}}> Computer Science</time>
+            <time className="post-date" style={{color: "#08fdd8"}}> HTML &amp; CSS</time>
             <div className="blog-post-title">
                 <h5>
-                    <a data-id="2354" href="/">
-                        Algorithms and Data Structures (Part 1)		
+                    <a data-id="1900" href="/">
+                        Ultimate Guide to CSS Grid Layout (Part 1)				
                     </a>
                 </h5>
             </div>
             <div className="blog-post-content">
-                For the last 20 years, Google has been the main search...		
+                Grid Container Grid Columns Grid Rows Grid Gaps &nbsp; CSS Grid...		<br />
+                <img src="https://www.shutterstock.com/image-illustration/css-code-on-dark-background-260nw-1893752428.jpg" 
+                alt="codeimage"
+                width="300" height="90"
+                />
+            </div> 
+        </div>
+    </article>
+    
+    <article className="post-articles" style={{borderTop: "2px solid orange"}}>
+        <div className="post-content">
+            <time className="post-date" style={{color: "orange"}}> JavaScript</time>
+            <div className="blog-post-title">
+                <h5>
+                    <a data-id="2037" href="/">
+                        How to Sort Arrays in JavaScript?				
+                    </a>
+                </h5>
+            </div>
+            <div className="blog-post-content">
+                In this article, we‟ll discuss how you can sort arrays in..<br />
+                <img src="https://cdn.pixabay.com/photo/2019/10/03/12/12/programmer-4523101__340.jpg" 
+                alt="codeimage"
+                width="300" height="90"
+                />		
             </div>
         </div>
     </article>
 
-    <article className="post-articles" style={{borderTop: "2px solid #f50057"}}>
-        <div className="post-content">
-            <time className="post-date" style={{color:"lightgrenn"}}> Web Development</time>
-            
-            <div className="blog-post-title">
-                <h5>
-                    <a data-id="2182" href="/">
-                        React JS Questions &/038; Answers (Part 1) 2021				
-                    </a>
-                </h5>
-            </div>
-            <div className="blog-post-content">
-                What is JSX? JSX is a superset of JavaScript that allows...		
-            </div>
-        </div>
-    </article>
+    
 
 
     <article className="post-articles" style={{borderTop: "2px solid orange"}}>
@@ -54,44 +62,118 @@ function Blog() {
                 </h5>
             </div>
             <div className="blog-post-content">
-                What are the possible ways to create objects in JavaScript? There...		
+                What are the possible ways to create objects in JavaScript? There..<br />
+                <img src="https://cdn.pixabay.com/photo/2016/03/27/18/54/technology-1283624__340.jpg" 
+                alt="codeimage"
+                width="300" height="60"
+                />			
             </div>
         </div>
     </article>
 
-   
-    <article className="post-articles" style={{borderTop: "2px solid orange"}}>
+    <article className="post-articles" style={{borderTop:"2px solid #81D8F7"}}>
         <div className="post-content">
-            <time className="post-date" style={{color: "orange"}}> JavaScript</time>
+            <time className="post-date" style={{color:"#81D8F7"}}> BootStrap</time>
             <div className="blog-post-title">
                 <h5>
-                    <a data-id="2037" href="/">
-                        How to Sort Arrays in JavaScript?				
+                    <a data-id="2301" href="/">
+                        Basic Concepts of BootStrap				
                     </a>
                 </h5>
             </div>
             <div className="blog-post-content">
-                In this article, we‟ll discuss how you can sort arrays in...		
+                BootStrap is a Front-end language and how do we use it..<br />
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMfnRghVIO5FyptncyVOsPNeWGAXE4ByQ8KFoSG5BWXA&usqp=CAU&ec=48600113.jpg" 
+                alt="codeimage"
+                width="300" height="90"
+                />	
+                
+            </div>
+        </div>
+    </article> 
+
+    <article className="post-articles" style={{borderTop: "2px solid #f50057"}}>
+        <div className="post-content">
+            <time className="post-date" style={{color:"lightgreen"}}> React js</time>
+            
+            <div className="blog-post-title">
+                <h5>
+                    <a data-id="2182" href="/">
+                        React JS is basically used to improve your website..			
+                    </a>
+                </h5>
+            </div>
+            <div className="blog-post-content">
+                What is React js? Why do we use package.json	<br />
+                <img src="https://cdn.pixabay.com/photo/2016/11/30/20/58/programming-1873854__340.png" 
+                alt="codeimage"
+                width="300" height="60"
+                />	
             </div>
         </div>
     </article>
-
-  
+    
     <article className="post-articles" style={{borderTop: "2px solid #08fdd8"}}>
         <div className="post-content">
-            <time className="post-date" style={{color: "#08fdd8"}}> HTML &amp; CSS</time>
+            <time className="post-date" style={{color:"#08fdd8"}}> Node Js</time>
             <div className="blog-post-title">
                 <h5>
-                    <a data-id="1900" href="/">
-                        Ultimate Guide to CSS Grid Layout (Part 1)				
+                    <a data-id="2019" href="/">
+                        How to use Node Js to improve your site?				
                     </a>
                 </h5>
             </div>
             <div className="blog-post-content">
-                Grid Container Grid Columns Grid Rows Grid Gaps &nbsp; CSS Grid...		
-            </div> 
+                How to use System file to improve your site...	<br />
+                <img src="https://cdn.pixabay.com/photo/2015/04/23/17/41/node-js-736399__340.png" 
+                alt="codeimage"
+                width="300" height="100"
+                />		
+            </div>    
         </div>
     </article>
+
+
+    <article className="post-articles" style={{borderTop: "2px solid #08fdd8"}}>
+        <div className="post-content">
+            <time className="post-date" style={{color: "#08fdd8"}}> Mongo DB</time>
+            <div className="blog-post-title">
+                <h5>
+                    <a data-id="2000" href="/">
+                        What is MongoDB and what does it do?				
+                    </a>
+                </h5>
+            </div>
+            <div className="blog-post-content">
+                MongoDB is a very popular Back-end language that allows developers to write..<br />
+                <img src="https://img.freepik.com/free-vector/digital-coding-background-with-numbers-zero-one_1017-30363.jpg?size=626&ext=jpg&ga=GA1.1.1020415244.1683548142&semt=sph.jpg" 
+                alt="codeimage"
+                width="300" height="90"
+                />		
+            </div>
+        </div>
+    </article>
+
+    <article className="post-articles" style={{borderTop: "2px solid #81D8F7"}}>
+        <div className="post-content">
+            <time className="post-date" style={{color:"#81D8F7"}}> Computer Science</time>
+            <div className="blog-post-title">
+                <h5>
+                    <a data-id="2354" href="/">
+                        Algorithms and Data Structures (Part 1)		
+                    </a>
+                </h5>
+            </div>
+            <div className="blog-post-content">
+                For the last 20 years, Google has been the main search...	<br />
+                <img src="https://images.unsplash.com/photo-1503252947848-7338d3f92f31?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80.jpg" 
+                alt="codeimage"
+                width="300" height="90"
+                />	
+            </div>
+        </div>
+    </article>
+
 
    
     <article className="post-articles" style={{borderTop: "2px solid #81D8F7"}}>
@@ -105,62 +187,15 @@ function Blog() {
                 </h5>
             </div>
             <div className="blog-post-content">
-                Number Representation: Formats Human beings have ten fingers. It is a...		
+                Number Representation: Formats Human beings have ten fingers. It is a..<br />
+                <img src="https://cdn.pixabay.com/photo/2017/01/10/23/01/code-1970468__340.png" 
+                alt="codeimage"
+                width="300" height="85"
+                />	
     </div>
 
         </div>
-    </article>
-
-  
-    <article className="post-articles" style={{borderTop: "2px solid #08fdd8"}}>
-        <div className="post-content">
-            <time className="post-date" style={{color:"#08fdd8"}}> HTML &amp; CSS</time>
-            <div className="blog-post-title">
-                <h5>
-                    <a data-id="2019" href="/">
-                        How to use System Fonts in CSS to improve your site?				
-                    </a>
-                </h5>
-            </div>
-            <div className="blog-post-content">
-                How to use System Fonts in CSS to improve your site...		
-            </div>    
-        </div>
-    </article>
-
-
-    <article className="post-articles" style={{borderTop: "2px solid #08fdd8"}}>
-        <div className="post-content">
-            <time className="post-date" style={{color: "#08fdd8"}}> HTML &amp; CSS</time>
-            <div className="blog-post-title">
-                <h5>
-                    <a data-id="2000" href="/">
-                        What is PostCSS and what does it do?				
-                    </a>
-                </h5>
-            </div>
-            <div className="blog-post-content">
-                PostCSS is a very popular tool that allows developers to write...		
-            </div>
-        </div>
-    </article>
-
-
-    <article className="post-articles" style={{borderTop:"2px solid #81D8F7"}}>
-        <div className="post-content">
-            <time className="post-date" style={{color:"#81D8F7"}}> Computer Science</time>
-            <div className="blog-post-title">
-                <h5>
-                    <a data-id="2301" href="/">
-                        Basic Concepts of Data Processing (Part 2)				
-                    </a>
-                </h5>
-            </div>
-            <div className="blog-post-content">
-                Languages, Syntax, and Semantics Now that we understand that machines work...		
-            </div>
-        </div>
-    </article>            
+    </article>           
     </div>
     
 </section>
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Sahil from "../asset/Sahil-sitaishi1.jpg";
+import "./font.css";
 function Title(){
-   
+    
     return(
         <div className="container">
             
         <div className="row text-center align-item-center" >
-            <div className="col-12 col-md-4 py-5 my-3">
+             <div className="col-12 col-md-4 py-5 my-3"> 
             <img
                  className="img-fluid rounded-circle img-center w-50"
                   src={Sahil}
@@ -15,17 +16,18 @@ function Title(){
                 
             </div>
             <div className="col-12 col-md-6 py-5 my-3">
-
+           
+           <div className="font">h6</div>
             
-            <div className="font-weight-light text-info" style={{ fontSize : "60px" }}>
-                {/* Hi I'm <span className="text-info">Sahil </span> */}
+             <div className="font-weight-light text-info" style={{ fontSize : "60px" }}>
+                
                 Hi I'm Sahil
             </div>
             <h4 className="font-weight-light">Full Stack Developer</h4>
             
         </div>
         </div>
-    </div>
+     </div>
     );
 }
 export default Title;
